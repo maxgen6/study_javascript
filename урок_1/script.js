aeb737f701ce37b428464ef3370000dc809633ca
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-03-27 14:50:46 +0300

    Поправил оформление

--- урок_1/script.js
+++ урок_1/script.js
@@ -1,24 +1,26 @@
 let money,income,addExpenses ,
-deposit,mission,period, budgetDay  ;
+deposit,mission,period, budgetDay , num;
 
-money = 1300;
-income = 'f600';
-addExpenses = 'TAXI, FOOD, CLOTHES, HOBBY, CINEMA';
-deposit = Boolean(200);
-mission = 2500;
-period = 12;
+    money = 1300;
+    income = 'f600';
+    addExpenses = 'TAXI, FOOD, CLOTHES, HOBBY, CINEMA';
+    deposit = Boolean(200);
+    mission = 2500;
+    period = 12;
 
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log('Период равен '+ period + ' месяцев.');
-console.log('Цель заработать:' + mission + ' рублей');
+    console.log(typeof money);
+    console.log(typeof income);
+    console.log(typeof deposit);
+    console.log('Период равен '+ period + ' месяцев.');
+    console.log('Цель заработать:' + mission + ' рублей');
 
 addExpenses = addExpenses.toLowerCase();
-console.log(addExpenses.split());
+    console.log(addExpenses.split());
 
 budgetDay = money / 30;
-console.log('Дневной бюджет: ' + budgetDay);
+    console.log('Дневной бюджет: ' + budgetDay);
+
+
 
      

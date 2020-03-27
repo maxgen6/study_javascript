01af821179bd2fd2bdeb47b7cd5a92770de9a6ff
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-03-27 14:47:08 +0300

    lesson02

--- урок_1/script.js
+++ урок_1/script.js
@@ -1,8 +1,24 @@
 let money,income,addExpenses ,
-deposit,mission,period  ;
+deposit,mission,period, budgetDay  ;
 
-alert('Hello world!');
+money = 1300;
+income = 'f600';
+addExpenses = 'TAXI, FOOD, CLOTHES, HOBBY, CINEMA';
+deposit = Boolean(200);
+mission = 2500;
+period = 12;
 
-console.log('Привет Мир!');
 
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log('Период равен '+ period + ' месяцев.');
+console.log('Цель заработать:' + mission + ' рублей');
 
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split());
+
+budgetDay = money / 30;
+console.log('Дневной бюджет: ' + budgetDay);
+
+     

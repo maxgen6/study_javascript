2ec06b0904dcf6ca81ba9527cbfadba90c812109
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-03-30 21:46:01 +0300

    последние исправления

--- урок_1/script.js
+++ урок_1/script.js
@@ -1,7 +1,10 @@
-let  money = 1300,
+'use strict';
+
+
+let  money,
     income = 'f600',
-    addExpenses = 'TAXI, FOOD, CLOTHES, HOBBY, CINEMA',
-    deposit = true,
+    addExpenses,
+    deposit,
     mission = 2500,
     period = 12;
 
@@ -12,15 +15,48 @@ let  money = 1300,
     console.log('Период равен '+ period + ' месяцев.');
     console.log('Цель заработать:' + mission + ' рублей');
 
-addExpenses = addExpenses.toLowerCase();
+
+
+money = prompt('Ваш месячный доход?');
+    console.log(money);
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
     console.log(addExpenses.split());
 
-budgetDay = money / 30;
-    console.log('Дневной бюджет: ' + budgetDay);
+deposit = confirm('Есть ли у вас депозит в банке?');
+    console.log(deposit);
 
-   
+let expenses1 = prompt('Введите обязательную статью расходов? ');
+    
+let expenses2 = prompt('Введите обязательную статью расходов? ');
+    
+let amount1 = prompt('Во сколько это обойдется? ');
+    
+let amount2 = prompt('Во сколько это обойдется? ');
+    
 
+let budgetMonth = parseInt(money) - (parseInt(amount1) + parseInt(amount2));
+    console.log(budgetMonth);
 
+let aim = Math.ceil(mission / budgetMonth);
+    console.log('Цель будет достигнута примерно через: ' + aim + ' месяцев.');
+
+let budgetDay = Math.floor(budgetMonth / 30);
+    if(budgetDay <= 0){
+        console.log('Без денег!');
+    } else{
+        console.log('Бюджет на день: ' +  budgetDay);
+    }
+
+if (budgetDay > 1200){
+    console.log('У вас высокий уровень дохода!');
+}  else if(1200 > budgetDay > 600){
+    console.log('У вас средний уровень дохода');
+}  else if (600 > budgetDay > 0){
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+}  else {
+    console.log('Что-то пошло не так');
+}
 
 
 

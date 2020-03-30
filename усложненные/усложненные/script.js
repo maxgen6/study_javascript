a07d18d9a227ad4d729c35bef9537e6713d40e41
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-03-30 21:41:33 +0300

    Дополнен синтаксис

--- усложненные/усложненные/script.js
+++ усложненные/усложненные/script.js
@@ -1,15 +1,14 @@
-num = 266219;
-let i;
+let num = 266219;
 let multiply = 1;
-let multiply_2;
 
 num = num.toString();
 
-for (i = 0; i<num.length; i++){
+for (let i = 0; i < num.length; i++){
     multiply *= num[i];
     console.log(multiply);
 }
-multiply_2 = multiply ** 3; 
-console.log('После возведения в степень: ' + multiply_2 );
-multiply_2 = multiply_2.toString();
-console.log('Первые 2 цифры: ' + multiply_2.substr( 0,2 ));
+
+let multiply2 = multiply ** 3; 
+console.log('После возведения в степень: ' + multiply2 );
+multiply2 = multiply2.toString();
+console.log('Первые 2 цифры: ' + multiply2.substr(0, 2));

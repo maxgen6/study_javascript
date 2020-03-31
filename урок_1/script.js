6ded237ce440ae162d4356c9e8ae4e07a826cd67
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-03-31 11:45:13 +0300

    Исправленное дз

--- урок_1/script.js
+++ урок_1/script.js
@@ -1,47 +1,36 @@
 'use strict';
 
 
-let  money,
+let  money, expenses1, expenses2, amount1, amount2, budgetMonth, aim,
+    budgetDay,
     income = 'f600',
     addExpenses,
     deposit,
     mission = 2500,
     period = 12;
 
-
-    console.log(typeof money);
-    console.log(typeof income);
-    console.log(typeof deposit);
     console.log('Период равен '+ period + ' месяцев.');
     console.log('Цель заработать:' + mission + ' рублей');
 
-
-
 money = prompt('Ваш месячный доход?');
-    console.log(money);
-
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-    console.log(addExpenses.split());
-
 deposit = confirm('Есть ли у вас депозит в банке?');
-    console.log(deposit);
+expenses1 = prompt('Введите обязательную статью расходов? ');   
+expenses2 = prompt('Введите обязательную статью расходов? ');    
+amount1 = prompt('Во сколько это обойдется? ');   
+amount2 = prompt('Во сколько это обойдется? ');
 
-let expenses1 = prompt('Введите обязательную статью расходов? ');
-    
-let expenses2 = prompt('Введите обязательную статью расходов? ');
-    
-let amount1 = prompt('Во сколько это обойдется? ');
-    
-let amount2 = prompt('Во сколько это обойдется? ');
-    
+console.log(money);
+console.log(addExpenses.split(','));
+console.log(deposit);
 
-let budgetMonth = parseInt(money) - (parseInt(amount1) + parseInt(amount2));
-    console.log(budgetMonth);
+budgetMonth = parseInt(money) - (parseInt(amount1) + parseInt(amount2));
+console.log(budgetMonth);
 
-let aim = Math.ceil(mission / budgetMonth);
-    console.log('Цель будет достигнута примерно через: ' + aim + ' месяцев.');
+aim = Math.ceil(mission / budgetMonth);
+console.log('Цель будет достигнута примерно через: ' + aim + ' месяцев.');
 
-let budgetDay = Math.floor(budgetMonth / 30);
+budgetDay = Math.floor(budgetMonth / 30);
     if(budgetDay <= 0){
         console.log('Без денег!');
     } else{

67289884479e42dc2733587ca044208af616f67e
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-01 21:56:02 +0300

    5-е дз

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 'use strict';
 
+let isNumber = function(n){
+    return (parseFloat(n)) && isFinite(n);
+}
 
-let aim,
-    budgetDay,
+
+let budgetDay,
+    money,
     income = 'freelance',
     mission = 2500,
     period = 12,
-    money = prompt('Ваш месячный доход?'),
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    expenses1 = prompt('Введите обязательную статью расходов? '),   
-    expenses2 = prompt('Введите обязательную статью расходов? '),    
-    amount1 = prompt('Во сколько это обойдется? '),   
-    amount2 = prompt('Во сколько это обойдется? '),
+    deposit = confirm('Есть ли у вас депозит в банке?'),   
     accumulatedMonth = 0;
 
 
 console.log(addExpenses.split(','));
 
-
-
+let start = function(){
+    
+    do{
+        money = prompt('Ваш месячный доход?');
+    }
+    while(!isNumber(money))
+}
+start();
+console.log('Доходы за месяц: ' + money);
 
 let showTypeOf = function(data){
     console.log(typeof(data));
@@ -29,14 +35,31 @@ showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
+let expenses = [];
 
-function getExpensesMonth(){
-    return parseInt(amount1) + parseInt(amount2);
-}
-console.log(getExpensesMonth());
+let getExpensesMonth = function (){
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++){
+
+    expenses[i] = prompt('Введите обязательную статью расходов? ');
+    if(!isNumber(sum[i])){
+    sum += +prompt('Во сколько это обойдется? ');
+    }
+    
+    }
+    console.log(expenses);
+    return sum;
+ 
+};
+
+let expensesAmount = getExpensesMonth();
+
+console.log('Расходы за месяц: ' + expensesAmount);
+
 
 function getAccumulatedMonth(){
-    return parseInt(money) - (parseInt(amount1) + parseInt(amount2));
+    return money - expensesAmount;
 }
 accumulatedMonth  = getAccumulatedMonth();
 
@@ -57,10 +80,10 @@ let getStatusIncome = function(){
         return ('У вас высокий уровень дохода!');
     }  else if((1200 > budgetDay) && (budgetDay >= 600)){
         return ('У вас средний уровень дохода');
-    }  else if ((600 > budgetDay)&& (budgetDay > 0)){
+    }  else if ((600 > budgetDay) && (budgetDay > 0)){
         return ('К сожалению у вас уровень дохода ниже среднего');
     }  else {
-        return ('Что-то пошло не так');
+        return ('Цель не будет достигнута');
     }
 
 }

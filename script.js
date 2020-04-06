0b6f47f6cc9dd8efa3bab4a71007666d43335b08
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-06 19:59:11 +0300

    сделанное дз7

--- script.js
+++ script.js
@@ -40,13 +40,12 @@ let appData = {
                 appData.expenses[exp[i]] = count; 
                 
             }
-            console.log(appData.expenses);
+            
             
 
     }
     };
 //  let accumulatedMonth = 0;
-console.log('Доходы за месяц: ' + money);
 // console.log(appData);
 
 appData.asking();
@@ -54,47 +53,41 @@ appData.asking();
 
  appData.getExpensesMonth = function (){
     for (let key in appData.expenses){
-
-        console.log(appData.expenses[key]);
         appData.expensesMonth += +appData.expenses[key];
         }
-        console.log(appData.expensesMonth);
-
 };
-console.log(appData.expensesMonth);
 // appData.getExpensesMonth = getExpensesMonth;
 
 
-let expensesAmount = appData.getExpensesMonth();
-console.log('Расходы за месяц: ' + expensesAmount);
-
+appData.getExpensesMonth();
+console.log('Расходы за месяц: ' + appData.expensesMonth);
+// console.log(typeof appData.expensesMonth);
 
  appData.getBudget = function(){
-    return money - expensesAmount;
+    return money - appData.expensesMonth;
 }
-appData.getBudget = getBudget;
-accumulatedMonth  = appData.getBudget();
-console.log(accumulatedMonth);
-function getTargetMonth(){
-    return appData.mission / accumulatedMonth;
+
+appData.budgetMonth = appData.getBudget();
+appData.getTargetMonth = function (){
+    return appData.mission / appData.budgetMonth;
 }
-appData.getTargetMonth = getTargetMonth;
+
 
 if(appData.getTargetMonth() > 0){
 console.log('Цель будет достигнута за: ' + Math.ceil(appData.getTargetMonth()) + ' месяцев');
-}else{
+} else{
     console.log('Цель не будет достигнута');
 }
 
-appData.budgetDay = Math.floor(accumulatedMonth / 30);
-    if(appData.budgetDay <= 0){
-        console.log('Без денег!');
-    } else{
-        console.log('Бюджет на день: ' +  appData.budgetDay);
-    }
+// appData.budgetDay = Math.floor(appData.budgetMonth / 30);
+//     if(appData.budgetDay <= 0){
+//         console.log('Без денег!');
+//     } else{
+//         console.log('Бюджет на день: ' +  appData.budgetDay);
+//     }
  
 
-let getStatusIncome = function(){
+appData.getStatusIncome = function(){
     if (appData.budgetDay >= 1200){
         return ('У вас высокий уровень дохода!');
     }  else if((1200 > appData.budgetDay) && (appData.budgetDay >= 600)){
@@ -106,7 +99,6 @@ let getStatusIncome = function(){
     }
 
 }
-appData.getStatusIncome = getStatusIncome;
 console.log(appData.getStatusIncome());
 
 
